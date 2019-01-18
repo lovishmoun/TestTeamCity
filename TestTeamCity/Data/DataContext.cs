bd1ee7a8498e1ba503aa73278e699de13c72c760
lovishmoun <32538489+lovishmoun@users.noreply.github.com>
2019-01-18 13:03:45 +0530

    Add files via upload

---
+++ TestTeamCity/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestTeamCity.Model;
+
+namespace TestTeamCity.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestTeamCity.Model.Product> Product { get; set; }
+    }
+}
